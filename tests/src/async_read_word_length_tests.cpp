23794f4190f90f288f0f8bd7c745d04126f5276f
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-26 01:03:15 +0300

    Test words length reading asynchronous operation

---
+++ tests/src/async_read_word_length_tests.cpp
@@ -0,0 +1,46 @@
+#include "tikpp/tests/fakes/socket.hpp"
+#include "tikpp/tests/fixtures/socket.hpp"
+
+#include "tikpp/detail/operations/async_read_word_length.hpp"
+#include "tikpp/request.hpp"
+
+#include "gtest/gtest.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
+
+namespace tikpp::tests {
+
+struct AsyncReadWordLengthTest : tikpp::tests::fixtures::socket {};
+
+TEST_F(AsyncReadWordLengthTest, LengthTest) {
+    constexpr auto test_iterations = 100;
+
+    std::vector<std::uint8_t> buf {};
+    std::time_t               start_seed {std::time(nullptr)};
+    std::srand(start_seed);
+
+    for (std::size_t i {0}; i < test_iterations; ++i) {
+        tikpp::detail::encode_length(std::rand(), buf);
+    }
+
+    sock.input_buffer().write(reinterpret_cast<char *>(buf.data()), buf.size());
+    std::srand(start_seed);
+
+    for (std::size_t i {0}; i < test_iterations; ++i) {
+        tikpp::detail::operations::async_read_word_length(
+            sock, [](const auto &err, auto len) {
+                EXPECT_FALSE(err);
+                EXPECT_EQ(rand(), len);
+            });
+
+        io.restart();
+        io.run();
+    }
+}
+
+} // namespace tikpp::tests
