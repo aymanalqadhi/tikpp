72850294e600cf09a447cb870236662ff5ee7abf
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-26 15:01:42 +0300

    Test the asynchronous connect operation

---
+++ tests/src/async_connect_tests.cpp
@@ -0,0 +1,46 @@
+#include "tikpp/detail/operations/async_connect.hpp"
+#include "tikpp/tests/fakes/socket.hpp"
+
+#include "gtest/gtest.h"
+#include <boost/asio/async_result.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+namespace {
+
+constexpr auto invalid_ip_address = "Invalid address";
+constexpr auto valid_ip_address   = "127.0.0.1";
+constexpr auto api_port           = 8728;
+
+} // namespace
+
+namespace tikpp::tests {
+
+TEST(AsyncConnectTest, ValidityTest) {
+    boost::asio::io_context io {};
+
+#if defined(NO_MOCK_OBJECTS)
+    boost::asio::ip::tcp::socket sock {io};
+#else
+    tikpp::tests::fakes::socket sock {io};
+#endif
+
+    ASSERT_FALSE(sock.is_open());
+
+    tikpp::detail::operations::async_connect(
+        sock, ::invalid_ip_address, ::api_port, [&sock](const auto &err) {
+            EXPECT_TRUE(err);
+            EXPECT_FALSE(sock.is_open());
+        });
+    io.run();
+    EXPECT_FALSE(sock.is_open());
+
+    tikpp::detail::operations::async_connect(
+        sock, ::valid_ip_address, ::api_port, [&sock](const auto &err) {
+            EXPECT_FALSE(err);
+            EXPECT_TRUE(sock.is_open());
+        });
+    io.run();
+    EXPECT_TRUE(sock.is_open());
+}
+
+} // namespace tikpp::tests
