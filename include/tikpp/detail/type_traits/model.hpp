c97940dad77065a1138bdf05c6c703daef013e19
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-29 23:01:14 +0300

    Remove unneeded meta functions

--- include/tikpp/detail/type_traits/model.hpp
+++ include/tikpp/detail/type_traits/model.hpp
@@ -12,14 +12,6 @@ namespace tikpp::detail::type_traits {
 HAS_MEMBER_FUNCTION_FULL(
     is_hash_map, operator[], std::string &,(std::declval<const std::string &>()));
 
-HAS_MEMBER_FUNCTION_FULL(has_create,
-                         create,
-                         T,
-                         (std::declval<std::map<std::string, std::string>>()));
-
-HAS_MEMBER_FUNCTION(dissolve,
-                    (std::declval<std::map<std::string, std::string>>()));
-
 } // namespace tikpp::detail::type_traits
 
 #endif
