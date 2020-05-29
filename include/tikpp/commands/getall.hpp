231ad0c03c711814db55bc5c1074890e2cfc3d55
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-29 22:59:48 +0300

    Make getall command generic

--- include/tikpp/commands/getall.hpp
+++ include/tikpp/commands/getall.hpp
@@ -1,13 +1,25 @@
+#include "tikpp/models/convert.hpp"
 #include "tikpp/request.hpp"
+
+#include "fmt/format.h"
+#include <boost/algorithm/string.hpp>
 
 #include <cstdint>
 #include <string>
 
 namespace tikpp::commands {
 
+template <typename Model>
 struct getall : tikpp::request {
-    getall(std::uint32_t tag, const std::string &path)
-        : request {path + command_suffix, tag} {
+    getall(std::uint32_t tag)
+        : request {std::string {Model::api_path} + command_suffix, tag} {
+        tikpp::models::proplist_collector collector {};
+
+        Model m {};
+        m.convert(collector);
+
+        auto proplist = boost::join(collector.proplist, ",");
+        add_param(".proplist", std::move(proplist));
     }
 
     static constexpr auto command_suffix = "/getall";
