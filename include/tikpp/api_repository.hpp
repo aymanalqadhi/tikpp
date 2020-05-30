fa893632cf1d421e46a0761a5de38f56bc5f3e6c
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-30 17:59:28 +0300

    Add a method to load items with a query

--- include/tikpp/api_repository.hpp
+++ include/tikpp/api_repository.hpp
@@ -5,27 +5,45 @@
 #include "tikpp/commands/getall.hpp"
 #include "tikpp/detail/async_result.hpp"
 #include "tikpp/models/convert.hpp"
+#include "tikpp/models/query.hpp"
 
 #include <boost/system/error_code.hpp>
 
 #include <memory>
+#include <string>
 #include <type_traits>
+#include <vector>
 
 namespace tikpp {
 
 template <typename Model, typename ApiPtr>
 struct api_repository {
+    using query_type = decltype(std::declval<tikpp::models::query>().words);
+
     explicit api_repository(ApiPtr api) : api_ {std::move(api)} {
     }
 
     template <typename CompletionToken>
-    decltype(auto) load_all(CompletionToken &&token) {
+    inline decltype(auto) load_all(CompletionToken &&token) {
+        auto req =
+            api_->template make_request<tikpp::commands::getall<Model>>();
+        return do_load(std::move(req), std::forward<CompletionToken>(token));
+    }
+
+    template <typename CompletionToken>
+    inline decltype(auto) load(query_type query, CompletionToken &&token) {
+        auto req = api_->template make_request<tikpp::commands::getall<Model>>(
+            std::move(query));
+        return do_load(std::move(req), std::forward<CompletionToken>(token));
+    }
+
+  private:
+    template <typename CompletionToken>
+    decltype(auto) do_load(std::shared_ptr<tikpp::request> req,
+                           CompletionToken &&              token) {
         GENERATE_COMPLETION_HANDLER(
             void(const boost::system::error_code &, std::vector<Model> &&),
             token, handler, result)
-
-        auto req =
-            api_->template make_request<tikpp::commands::getall<Model>>();
 
         api_->async_send(
             std::move(req), [handler {std::move(handler)},
@@ -53,7 +71,6 @@ struct api_repository {
         return result.get();
     }
 
-  private:
     ApiPtr api_;
 };
 
