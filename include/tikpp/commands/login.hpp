95885ef82cad35a8dabcb732b4706e2a929b3d52
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-31 14:10:17 +0300

    Remove the first login step for v1 API

--- include/tikpp/commands/login.hpp
+++ include/tikpp/commands/login.hpp
@@ -8,24 +8,18 @@ namespace tikpp::commands {
 
 namespace v1 {
 
-struct login1 : tikpp::request {
-    login1(std::uint32_t tag) : request {command, tag} {
-    }
-
-    static constexpr auto command = "/login";
-};
-
-struct login2 final : login1 {
-    login2(std::uint32_t      tag,
-           const std::string &name,
-           const std::string &password,
-           const std::string &cha)
-        : login1 {tag} {
+struct login final : tikpp::request {
+    login(std::uint32_t      tag,
+          const std::string &name,
+          const std::string &password,
+          const std::string &cha)
+        : request {command, tag} {
         add_param(name_param, name);
         add_param(password_param,
                   fmt::format("00{}", tikpp::hash_password(password, cha)));
     }
 
+    static constexpr auto command         = "/login";
     static constexpr auto name_param      = "name";
     static constexpr auto password_param  = "response";
     static constexpr auto challenge_param = "ret";
