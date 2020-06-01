f564bb95fe68ed24fbfb2799ede87891583aff73
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-01 20:02:52 +0300

    Make set command accept models by value

--- include/tikpp/commands/set.hpp
+++ include/tikpp/commands/set.hpp
@@ -1,7 +1,7 @@
 #ifndef TIKPP_COMMANDS_SET_HPP
 #define TIKPP_COMMANDS_SET_HPP
 
-#include "tikpp/models/convert.hpp"
+#include "tikpp/models/converters.hpp"
 #include "tikpp/request.hpp"
 
 #include <cstdint>
@@ -10,7 +10,7 @@ namespace tikpp::commands {
 
 template <typename Model>
 struct set : tikpp::request {
-    set(std::uint32_t tag, Model &&model)
+    set(std::uint32_t tag, Model model)
         : request {std::string {Model::api_path} + command_suffix, tag} {
         tikpp::models::dissolver<set<Model>> dissolver {*this};
         model.convert(dissolver);
