1dd07034aab9ad0e56b57d127d853c7a6e6bc1d7
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-01 20:07:38 +0300

    Test proplist collector converter

---
+++ tests/src/proplist_collector_tests.cpp
@@ -0,0 +1,34 @@
+#include "tikpp/models/converters.hpp"
+#include "tikpp/tests/fakes/model.hpp"
+
+#include "gtest/gtest.h"
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+namespace tikpp::tests {
+
+TEST(PropListTests, CollectingTest) {
+    tikpp::tests::fakes::model                                  model {};
+    tikpp::models::proplist_collector<std::vector<std::string>> collector {};
+
+    model.convert(collector);
+
+    const auto &proplist = collector.proplist;
+
+    auto contains = [&proplist](const std::string &key) -> bool {
+        auto itr = std::find(proplist.begin(), proplist.end(), key);
+        return itr != proplist.end();
+    };
+
+    EXPECT_TRUE(contains("prop1"));
+    EXPECT_TRUE(contains("prop2"));
+    EXPECT_TRUE(contains("prop3"));
+    EXPECT_TRUE(contains("prop5"));
+    EXPECT_TRUE(contains("prop6"));
+    EXPECT_FALSE(contains("prop7"));
+    EXPECT_FALSE(contains("prop8"));
+}
+
+} // namespace tikpp::tests
