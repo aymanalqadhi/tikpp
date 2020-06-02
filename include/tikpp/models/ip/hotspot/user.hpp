b3da9340b90adb963bc2946d57b3321ed912f835
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-02 13:03:38 +0300

    Add a basic detail view for hotspot users

--- include/tikpp/models/ip/hotspot/user.hpp
+++ include/tikpp/models/ip/hotspot/user.hpp
@@ -2,12 +2,14 @@
 #define TIKPP_MODELS_IP_HOTSPOT_USER_HPP
 
 #include "tikpp/models/model.hpp"
+#include "tikpp/models/types/one_way.hpp"
 
+#include <cstdint>
 #include <string>
 
 namespace tikpp::models::ip::hotspot {
 
-struct user final : tikpp::models::model {
+struct user : tikpp::models::model {
     static constexpr auto api_path = "/ip/hotspot/user";
 
     std::string name;
@@ -24,6 +26,24 @@ struct user final : tikpp::models::model {
         c["password"] % password;
         c["profile"] % profile;
         c["disabled"] % disabled;
+    }
+};
+
+struct user_detail : user {
+    tikpp::models::types::one_way<std::uint64_t> bytes_in;
+    tikpp::models::types::one_way<std::uint64_t> bytes_out;
+
+    std::uint64_t limit_bytes_in;
+    std::uint64_t limit_bytes_out;
+
+    template <template <typename> typename Converter, typename Map>
+    inline void convert(Converter<Map> &c) {
+        user::convert(c);
+
+        c["bytes-in"] % bytes_in;
+        c["bytes-out"] % bytes_out;
+        c["limit-bytes-in"] % limit_bytes_in;
+        c["limit-bytes-out"] % limit_bytes_out;
     }
 };
 
