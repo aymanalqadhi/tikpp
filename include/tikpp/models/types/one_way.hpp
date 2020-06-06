c0348b23c4cc67190e926360c01a7278e8b035fc
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-07 01:01:54 +0300

    Extend arithmetic_wrapper and logic_wrapper on the one way binder wrapper

--- include/tikpp/models/types/one_way.hpp
+++ include/tikpp/models/types/one_way.hpp
@@ -1,24 +1,29 @@
 #ifndef TIKPP_MODELS_TYPES_READ_ONLY_HPP
 #define TIKPP_MODELS_TYPES_READ_ONLY_HPP
 
+#include "tikpp/models/types/arithmetic_wrapper.hpp"
+#include "tikpp/models/types/logic_wrapper.hpp"
+
 #include <utility>
 
 namespace tikpp::models::types {
 
 template <typename T>
-struct one_way {
-    one_way() = default;
-
-    one_way(T &&val) : value_ {std::forward<T>(val)} {
+struct one_way : tikpp::models::types::arithmetic_wrapper<one_way<T>, T>,
+                 tikpp::models::types::logic_wrapper<T> {
+    one_way(T &&value)
+        : value_ {std::move(value)},
+          arithmetic_wrapper<one_way<T>, T> {value_},
+          logic_wrapper<T> {value_} {
     }
 
-    inline operator const T &() const noexcept {
-        return value_;
+    one_way(const T &value) : one_way(static_cast<T>(value)) {
     }
 
-    inline auto value() const noexcept -> const T & {
-        return value_;
+    one_way() : one_way {T {}} {
     }
+
+    using arithmetic_wrapper<one_way<T>, T>::operator=;
 
   private:
     T value_;
