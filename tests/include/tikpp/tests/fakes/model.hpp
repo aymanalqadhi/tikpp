db2351dc98592555ac602e15a608e783340ce9f9
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-07 17:21:23 +0300

    Add a second fake model to test type wrappers

--- tests/include/tikpp/tests/fakes/model.hpp
+++ tests/include/tikpp/tests/fakes/model.hpp
@@ -1,13 +1,18 @@
 #ifndef TIKPP_TESTS_FAKES_MODEL_HPP
 #define TIKPP_TESTS_FAKES_MODEL_HPP
 
+#include "tikpp/models/types/identity.hpp"
+#include "tikpp/models/types/one_way.hpp"
+#include "tikpp/models/types/readonly.hpp"
+#include "tikpp/models/types/two_way.hpp"
+
 #include <cstdint>
 #include <string>
 
 namespace tikpp::tests::fakes {
 
-struct model {
-    static constexpr auto api_path = "/tikpp/tests/fakes/model";
+struct model1 {
+    static constexpr auto api_path = "/tikpp/tests/fakes/model1";
 
     std::string   prop1;
     bool          prop2;
@@ -24,6 +29,24 @@ struct model {
         c["prop4"] % prop4;
         c["prop5"] % prop5;
         c["prop6"] % prop6;
+    }
+};
+
+struct model2 {
+    static constexpr auto api_path = "/tikpp/tests/fakes/model2";
+
+    tikpp::models::types::readonly<tikpp::models::types::identity> id;
+
+    tikpp::models::types::two_way<std::string>  two_way_data;
+    tikpp::models::types::one_way<std::string>  one_way_data;
+    tikpp::models::types::readonly<std::string> readonly_data;
+
+    template <typename Convertor>
+    void convert(Convertor &c) {
+        c["id"] % id;
+        c["readonly-data"] % readonly_data;
+        c["one-way-data"] % one_way_data;
+        c["two-way-data"] % two_way_data;
     }
 };
 
