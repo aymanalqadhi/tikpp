223ced4a2cf73ad03dba48f9e42d73452ac9ba86
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-09 20:36:30 +0300

    Add the remaining ip/hotspot/user fields

--- include/tikpp/data/ip/hotspot/user.hpp
+++ include/tikpp/data/ip/hotspot/user.hpp
@@ -2,21 +2,20 @@
 #define TIKPP_DATA_IP_HOTSPOT_USER_HPP
 
 #include "tikpp/data/model.hpp"
-
 #include "tikpp/data/types/bytes.hpp"
 #include "tikpp/data/types/duration.hpp"
-#include "tikpp/data/types/wrapper.hpp"
 
 #include <chrono>
 #include <cstdint>
 #include <string>
 
-namespace tikpp ::data::ip::hotspot {
+namespace tikpp::data::ip::hotspot {
 
 struct user : tikpp::data::model {
     static constexpr auto api_path = "/ip/hotspot/user";
 
     read_write<std::string> name;
+    read_write<bool>        is_disabled;
     read_write<std::string> password;
     read_write<std::string> profile;
     read_write<std::string> comment;
@@ -26,6 +25,7 @@ struct user : tikpp::data::model {
         model::convert(c);
 
         c["name"] % name;
+        c["disabled"] % is_disabled;
         c["password"] % password;
         c["profile"] % profile;
         c["comment"] % comment;
@@ -41,16 +41,16 @@ struct user_detail : user {
     read_only<std::uint32_t> packets_in;
     read_only<std::uint32_t> packets_out;
     read_only<duration>      uptime;
-    read_only<bool>          is_dynamic;
-
-    read_write<bytes>    limit_bytes_in;
-    read_write<bytes>    limit_bytes_out;
-    read_write<bytes>    limit_bytes_total;
-    read_write<duration> limit_uptime;
 
     read_write<std::string> address;
-    read_write<std::string> mac_address;
     read_write<std::string> email;
+    read_write<bytes>       limit_bytes_in;
+    read_write<bytes>       limit_bytes_out;
+    read_write<bytes>       limit_bytes_total;
+    read_write<duration>    limit_uptime;
+    read_write<std::string> mac_address;
+    read_write<std::string> routes;
+    read_write<std::string> server;
 
     template <typename Converter>
     inline void convert(Converter &c) {
@@ -61,16 +61,16 @@ struct user_detail : user {
         c["packets-in"] % packets_in;
         c["packets-out"] % packets_out;
         c["uptime"] % uptime;
-        c["dynamic"] % is_dynamic;
 
+        c["address"] % address;
+        c["email"] % email;
         c["limit-bytes-in"] % limit_bytes_in;
         c["limit-bytes-out"] % limit_bytes_out;
         c["limit-bytes-total"] % limit_bytes_total;
         c["limit-uptime"] % limit_uptime;
-
-        c["address"] % address;
         c["mac-address"] % mac_address;
-        c["email"] % email;
+        c["routes"] % routes;
+        c["server"] % server;
     }
 };
 
