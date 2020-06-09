afb6844bc4af46687686190e337bd517b19cfe40
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-09 03:09:37 +0300

    Add the rest of ip/hotspot/user fields

--- include/tikpp/data/ip/hotspot/user.hpp
+++ include/tikpp/data/ip/hotspot/user.hpp
@@ -19,6 +19,7 @@ struct user : tikpp::data::model {
     two_way<std::string> name;
     two_way<std::string> password;
     two_way<std::string> profile;
+    two_way<std::string> comment;
 
     template <typename Converter>
     inline void convert(Converter &c) {
@@ -27,6 +28,7 @@ struct user : tikpp::data::model {
         c["name"] % name;
         c["password"] % password;
         c["profile"] % profile;
+        c["comment"] % comment;
     }
 };
 
@@ -34,15 +36,21 @@ struct user_detail : user {
     using bytes    = tikpp::data::types::bytes;
     using duration = tikpp::data::types::duration<std::chrono::seconds>;
 
-    one_way<bytes>    bytes_in;
-    one_way<bytes>    bytes_out;
-    one_way<duration> uptime;
-    one_way<bool>     is_dynamic;
+    one_way<bytes>         bytes_in;
+    one_way<bytes>         bytes_out;
+    one_way<std::uint32_t> packets_in;
+    one_way<std::uint32_t> packets_out;
+    one_way<duration>      uptime;
+    one_way<bool>          is_dynamic;
 
     two_way<bytes>    limit_bytes_in;
     two_way<bytes>    limit_bytes_out;
     two_way<bytes>    limit_bytes_total;
     two_way<duration> limit_uptime;
+
+    two_way<std::string> address;
+    two_way<std::string> mac_address;
+    two_way<std::string> email;
 
     template <typename Converter>
     inline void convert(Converter &c) {
@@ -50,6 +58,8 @@ struct user_detail : user {
 
         c["bytes-in"] % bytes_in;
         c["bytes-out"] % bytes_out;
+        c["packets-in"] % packets_in;
+        c["packets-out"] % packets_out;
         c["uptime"] % uptime;
         c["dynamic"] % is_dynamic;
 
@@ -57,6 +67,10 @@ struct user_detail : user {
         c["limit-bytes-out"] % limit_bytes_out;
         c["limit-bytes-total"] % limit_bytes_total;
         c["limit-uptime"] % limit_uptime;
+
+        c["address"] % address;
+        c["mac-address"] % mac_address;
+        c["email"] % email;
     }
 };
 
