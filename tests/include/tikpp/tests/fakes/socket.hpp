ce1d79d2259818e3839335c29ccf01d6ed1cf14b
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-11 20:19:23 +0300

    Reimplement the fake socket struct with pipes instead of string streams

--- tests/include/tikpp/tests/fakes/socket.hpp
+++ tests/include/tikpp/tests/fakes/socket.hpp
@@ -3,38 +3,43 @@
 
 #include "tikpp/io_context.hpp"
 
+#include <boost/asio/buffer.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/process/async_pipe.hpp>
 #include <boost/system/system_error.hpp>
 
 #include <cassert>
-#include <sstream>
 #include <stdexcept>
 
 namespace tikpp::tests::fakes {
 
 struct socket final {
-    using executor_type = tikpp::io_context::executor_type;
-
-    socket(tikpp::io_context &io)
-        : io_ {io}, connected_ {false}, fails_ {false} {
-    }
+    using executor_type = boost::asio::io_context::executor_type;
 
     inline auto get_executor() -> executor_type {
         return io_.get_executor();
     }
 
-    template <typename Handler>
-    inline void async_connect(const boost::asio::ip::tcp::endpoint &ep,
-                              Handler &&                            handler) {
-        assert(!connected_);
+    socket(boost::asio::io_context &io)
+        : io_ {io},
+          input_pipe_ {io_},
+          output_pipe_ {io_},
+          connected_ {false},
+          fails_ {false} {
+    }
 
-        if (fails_) {
+    template <typename CompletionHandler>
+    inline void async_connect(const boost::asio::ip::tcp::endpoint &ep,
+                              CompletionHandler &&                  handler) {
+        assert(!connected_.load());
+
+        if (fails_.load()) {
             handler(boost::asio::error::fault);
             return;
         }
 
-        connected_ = true;
-        ep_        = ep;
+        connected_.store(true);
+        ep_ = ep;
 
         handler(boost::system::error_code {});
     }
@@ -42,16 +47,16 @@ struct socket final {
     inline void close() {
         assert(connected_);
 
-        if (fails_) {
-            throw std::runtime_error {"Cannot close socket"};
+        if (fails_.load()) {
+            throw std::logic_error {"Cannot close socket"};
         }
 
-        connected_ = false;
+        connected_.store(false);
     }
 
-    template <typename Handler>
+    template <typename CompletionHandler>
     inline void async_read_some(boost::asio::mutable_buffer buf,
-                                Handler &&                  handler) {
+                                CompletionHandler &&        handler) {
         assert(connected_);
 
         if (fails_) {
@@ -59,14 +64,7 @@ struct socket final {
             return;
         }
 
-        auto nread =
-            ibuf_.readsome(static_cast<char *>(buf.data()), buf.size());
-
-        if (nread < 0) {
-            handler(boost::asio::error::eof, 0);
-        } else {
-            handler(boost::system::error_code {}, nread);
-        }
+        input_pipe_.async_read_some(buf, std::move(handler));
     }
 
     template <typename Handler>
@@ -79,20 +77,21 @@ struct socket final {
             return;
         }
 
-        obuf_.write(static_cast<const char *>(buf.data()), buf.size());
-        handler(boost::system::error_code {}, buf.size());
+        output_pipe_.async_write_some(buf, std::move(handler));
     }
 
     [[nodiscard]] inline auto is_open() const noexcept -> bool {
         return connected_;
     }
 
-    [[nodiscard]] inline auto input_buffer() noexcept -> std::stringstream & {
-        return ibuf_;
+    [[nodiscard]] inline auto input_pipe() noexcept
+        -> boost::process::async_pipe & {
+        return input_pipe_;
     }
 
-    [[nodiscard]] inline auto output_buffer() noexcept -> std::stringstream & {
-        return obuf_;
+    [[nodiscard]] inline auto output_pipe() noexcept
+        -> boost::process::async_pipe & {
+        return output_pipe_;
     }
 
     [[nodiscard]] inline auto remote_endpoint() noexcept
@@ -100,15 +99,13 @@ struct socket final {
         return ep_;
     }
 
-    inline void always_fail(bool val) noexcept {
-        fails_ = val;
-    }
-
   private:
-    tikpp::io_context &            io_;
-    bool                           connected_, fails_;
-    std::stringstream              ibuf_, obuf_;
+    tikpp::io_context &        io_;
+    boost::process::async_pipe input_pipe_;
+    boost::process::async_pipe output_pipe_;
+
     boost::asio::ip::tcp::endpoint ep_;
+    std::atomic_bool               connected_, fails_;
 };
 
 } // namespace tikpp::tests::fakes
