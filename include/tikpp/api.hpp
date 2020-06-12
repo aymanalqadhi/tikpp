4e93756517801ed360c8d9bc9a759a5bc69250fa
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-12 15:55:49 +0300

    Add a factory to make type-erased error handler API types

--- include/tikpp/api.hpp
+++ include/tikpp/api.hpp
@@ -5,14 +5,18 @@
 #include "tikpp/io_context.hpp"
 
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/system/error_code.hpp>
 
+#include <functional>
 #include <memory>
 #include <type_traits>
 
 namespace tikpp {
 
-template <typename ErrorHandle>
-using api = tikpp::basic_api<boost::asio::ip::tcp::socket, ErrorHandle>;
+template <typename ErrorHandler>
+using api = tikpp::basic_api<boost::asio::ip::tcp::socket, ErrorHandler>;
+
+using api_te = tikpp::basic_api_te<boost::asio::ip::tcp::socket>;
 
 template <typename AsyncStream = boost::asio::ip::tcp::socket,
           typename ErrorHandler>
@@ -29,6 +33,15 @@ template <typename AsyncStream = boost::asio::ip::tcp::socket,
     using wrapper_type = std::reference_wrapper<std::decay_t<ErrorHandler>>;
     return tikpp::make_basic_api<AsyncStream, wrapper_type>(
         io, wrapper_type {handler});
+}
+
+template <typename AsyncStream = boost::asio::ip::tcp::socket>
+[[nodiscard]] inline auto
+make_api_te(tikpp::io_context &                                    io,
+            std::function<void(const boost::system::error_code &)> handler)
+    -> std::shared_ptr<basic_api_te<AsyncStream>> {
+    return tikpp::make_basic_api<AsyncStream, std::decay_t<decltype(handler)>>(
+        io, std::move(handler));
 }
 
 } // namespace tikpp
