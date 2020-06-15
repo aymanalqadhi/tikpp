95200328c8c4a5a516eece8a84902cb33482b7ae
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-06-15 18:13:09 +0300

    Add a constructor to construct the command from identity object

--- include/tikpp/commands/remove.hpp
+++ include/tikpp/commands/remove.hpp
@@ -2,6 +2,7 @@
 #define TIKPP_COMMANDS_REMOVE_HPP
 
 #include "tikpp/request.hpp"
+#include "tikpp/data/types/identity.hpp"
 
 #include <cstdint>
 
@@ -12,6 +13,11 @@ struct remove : tikpp::request {
     remove(std::uint32_t tag, std::string id)
         : request {std::string {Model::api_path} + command_suffix, tag} {
         add_param(".id", id);
+    }
+
+    remove(std::uint32_t tag, tikpp::data::types::identity id)
+        : request {std::string {Model::api_path} + command_suffix, tag} {
+        add_param(".id", id.to_string());
     }
 
     static constexpr auto command_suffix = "/remove";
